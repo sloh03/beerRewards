32fef022fc93e8d11a01a51a1d0078292bef03a9
Stefanie Loh <stefanieloh@18AZCOPT030.local>
2018-03-17 12:59:33 -0700

    Events auto display

--- javascript/events.js
+++ javascript/events.js
@@ -1,5 +1,51 @@
 $(document).ready(function(){
 
+    function autoDisplayEvents() {
+
+        var eventType1 = $("#workout-event").val();
+
+        var queryURL = "http://api.eventful.com/json/events/search?app_key=J5kvbtGnvrsF2MwM&keywords=" + eventType1 + "&location=Tucson&date=Future";
+
+        $.ajax({
+            url: 'https://corsbridge.herokuapp.com/' + encodeURIComponent(queryURL),
+            method: "GET",
+            dataType: 'json'
+        }).then(function (response) {
+
+            console.log(response);
+
+            var eventful = response.events.event;
+
+            for (var i = 0; i < 10; i++) {
+
+                eventTitle = eventful[i].title;
+                console.log("Title: " + eventful[i].title);
+
+                eventVenue = eventful[i].venue_address;
+                console.log("Venue: " + eventful[i].venue_address);
+
+                startTime = eventful[i].start_time;
+                console.log("Starts: " + eventful[i].start_time);
+
+                eventDateString = moment(startTime).format('ddd, MMM Do, h:mm a');
+                console.log("Moment start time: " + eventDateString);
+
+                // var eventLink = eventful[i].url;
+                // console.log("Link: " + eventLink);
+
+                // var eventLinkContainer = $("<a>")
+                // eventLinkContainer.append(eventLink);
+                // console.log(eventLinkContainer);
+
+                displayEvents();
+            }
+        });
+
+    }
+
+    autoDisplayEvents();
+
+
     // AJAX call to get event info.
     $("#find-event").on("click", function(event) {
         event.preventDefault();
@@ -31,7 +77,7 @@ $(document).ready(function(){
                 startTime = eventful[i].start_time;
                 console.log("Starts: " + eventful[i].start_time);
 
-                var eventDateString = moment(startTime).format('ddd, MMM Do, h:mm a');
+                eventDateString = moment(startTime).format('ddd, MMM Do, h:mm a');
                 console.log("Moment start time: " + eventDateString);
 
                 // var eventLink = eventful[i].url;
@@ -51,7 +97,7 @@ $(document).ready(function(){
 
         if ( (eventTitle !== undefined) && (eventVenue !== undefined)) {
 
-            var row = $('<tr>').append('<td>' + '<p id="event-titles"><strong>' + eventTitle + '</strong>' + ' - ' + eventVenue + '<br>' + (moment(startTime).format('ddd, MMM Do, h:mm a')) + '<br>' + '</td>');
+            var row = $('<tr>').append('<td>' + '<p id="event-titles"><strong>' + eventTitle + '</strong>' + ' - ' + eventVenue + '<br>' + eventDateString + '<br>' + '</td>');
 
             $('#event-table > tbody').append(row);
         }
